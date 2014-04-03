a77bc9300670f6747d5fc97a571f76c006355bc4
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2014-04-03 19:46:00 +0200

    Add getHttpCode on Exception

--- Exception/ApiHttpResponseException.php
+++ Exception/ApiHttpResponseException.php
@@ -16,12 +16,26 @@ namespace Da\ApiClientBundle\Exception;
  */
 class ApiHttpResponseException extends \Exception
 {
+    protected $code;
+
     public function __construct($url, $code, $content)
     {
+        $this->code = $code;
+
         parent::__construct(sprintf('HTTP Api response error: (%s) %s %s',
             $code,
             $url,
             $content
         ));
+    }
+
+    /**
+     * GetHttpCode
+     *
+     * @return integer
+     */
+    public function getHttpCode()
+    {
+        return $this->code;
     }
 }
